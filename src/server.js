ebabf66f8faacbc59bb6b7a6c836ebd954a6b0ef
Oleh Mogylevsky <159718895+mogilevs@users.noreply.github.com>
2024-10-26 21:43:22 +0300

    add next

--- src/server.js
+++ src/server.js
@@ -13,7 +13,6 @@ export const startServer = () => {
   app.use(pino({ transport: { target: 'pino-pretty' } }));
 
   app.get('/contacts', async (req, res) => {
-    console.log('res in contacts', res);
     const contacts = await getAllContacts();
     res.status(200).json({
       status: 200,
@@ -35,15 +34,14 @@ export const startServer = () => {
     });
   });
 
-  app.use((err, req, res) => {
-    console.log('res in 500', err, req, res);
+  app.use((err, req, res, next) => {
     res.status(500).json({
       message: 'Something went wrong',
       error: err.message,
     });
   });
 
-  app.use('*', (req, res) => {
+  app.use('*', (req, res, next) => {
     res.status(404).json({
       message: 'Not found',
     });
