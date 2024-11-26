94005aed9265fe65456bade2c3c065b6d72921ee
Oleh Mogylevsky <159718895+mogilevs@users.noreply.github.com>
2024-11-26 13:17:44 +0200

    fix password check

--- src/services/auth.js
+++ src/services/auth.js
@@ -27,7 +27,7 @@ export const registerUser = async (payload) => {
 export const loginUser = async (payload) => {
   const user = await UserCollection.findOne({ email: payload.email });
   if (!user) throw createHttpError(404, 'User not found');
-  const isEqual = bcrypt.compare(payload.password, user.password);
+  const isEqual = await bcrypt.compare(payload.password, user.password);
   if (!isEqual) throw createHttpError(401, 'Unauthorized');
 
   await SessionsCollection.deleteOne({ userId: user._id });
